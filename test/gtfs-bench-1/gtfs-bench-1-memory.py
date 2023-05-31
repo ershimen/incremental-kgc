91884600031a1ad4c2089f8d9b706e52a318781d
ershimen <jan.cerezo6@gmail.com>
2023-06-01 01:11:51 +0200

    Updated tests

--- test/gtfs-bench-1/gtfs-bench-1-memory.py
+++ test/gtfs-bench-1/gtfs-bench-1-memory.py
@@ -4,19 +4,69 @@ sys.path.append('../../src/')
 
 __test__ = "gtfs-bench-1-memory"
 __mapping_file__ = "mapping.csv.ttl"
-__update_file__ = "data/AGENCY.csv"
 __update_data__ = {
-    "agency_id": "00000000000000100000",
-    "agency_name": "00000000000000000001",
-    "agency_url": "http://www.crtm.es",
-    "agency_timezone": "00000000000000000001",
-    "agency_lang": "00000000000000000001",
-    "agency_phone": "00000000000000000001",
-    "agency_fare_url": "https://www.crtm.es/billetes-y-tarifas",
+    "data/AGENCY.csv": {
+        "agency_id": "00000000000000100000",
+        "agency_name": "00000000000000000001",
+        "agency_url": "http://www.crtm.es",
+        "agency_timezone": "00000000000000000001",
+        "agency_lang": "00000000000000000001",
+        "agency_phone": "00000000000000000001",
+        "agency_fare_url": "https://www.crtm.es/billetes-y-tarifas",
+        }
     }
 __aux_data_path__ = '.aux'
 __snapshot_file__ = '.aux/snapshot.pkl'
 __keep_snapshot__ = False
+__method__ = 'memory'
+__engine__ = 'morph'
+
+def __store_data(file: str, data: object, extension: str):
+    """Stores 'data' to 'file' and returns the data in 'file' prior to the change.
+
+    Args:
+        file:
+            A file name.
+        data:
+            A data object (for example json). The types supported are the following:
+                .json: for .csv files
+        extension:
+            Extension of 'file'. This parameter determines how to open 'file' and the return type. 
+        
+    Returns:
+        The data in 'file' prior to the update. The type of the object returned depends 'extension':
+            .csv: returns pandas.DataFrame
+    """
+    if extension == '.csv':
+        df = pd.read_csv(file, dtype=str)
+        df_new = pd.concat([df, pd.DataFrame([data])])
+        df_new.to_csv(file, index=False)
+        return df
+    elif extension == '.json':
+        raise NotImplementedError(f'The file type {extension} is not supported yet!')
+    else:
+        raise NotImplementedError(f'The file type {extension} is not supported yet!')
+
+
+def __restore_data(file: str, data: object, extension: str):
+    """Restores 'data' to 'file'.
+
+    Args:
+        file:
+            A file name.
+        data:
+            A data object (for example json). The types supported are the following:
+                pandas.DataFrame: for .csv files
+        extension:
+            Extension of 'file'. This parameter determines how to store 'data' into 'file'. 
+    """
+    if extension == '.csv':
+        data.to_csv(file, index=False)
+    elif extension == '.json':
+        raise NotImplementedError(f'The file type {extension} is not supported yet!')
+    else:
+        raise NotImplementedError(f'The file type {extension} is not supported yet!')
+
 
 print("Running test %s" % __test__)
 
@@ -27,50 +77,64 @@ import os
 
 print("Loading new graph...")
 
-print("#" * 60)
-
 start = time.time()
 # Load new graph
-g = inc.load_kg_aux_to_mem(
-    aux_data_path=__aux_data_path__,
-    mapping_file=__mapping_file__,
-    snapshot_file=__snapshot_file__,
-    old_graph=None)
+g = inc.load_kg(mapping_file=__mapping_file__,
+                snapshot_file=__snapshot_file__,
+                aux_data_path=__aux_data_path__,
+                old_graph=None,
+                method=__method__,
+                engine=__engine__)
 end = time.time()
 
-print("#" * 60)
+new_version_time = end-start
+new_version_triples = len(g)
 
-print("Loaded new graph in %.2fs" % (end-start))
+print("Loaded new graph in %.2fs" % (new_version_time))
 
-print("Adding data to %s..." % __update_file__)
-agency_df = pd.read_csv(__update_file__, dtype=str)
-agency_df_new = pd.concat([agency_df, pd.DataFrame([__update_data__])])
-agency_df_new.to_csv(__update_file__, index=False)
-print("Added new data to %s." % __update_file__)
+print("Adding new data to data source...")
+updated_files = {}
+file_extensions = {}
+for file in __update_data__:
+    _, extension = os.path.splitext(file)
+    file_extensions[file] = extension
+    print("\tAdding data to %s... " % file, end='')
+    updated_files[file] = __store_data(file=file,
+                                       data=__update_data__[file],
+                                       extension=extension)
+    print("OK")
+
+print("Finished adding new data to source.")
 
 print("Adding new triples...")
 
-print("#" * 60)
-
 start = time.time()
-g = inc.load_kg_aux_to_mem(
-    aux_data_path=__aux_data_path__,
-    mapping_file=__mapping_file__,
-    snapshot_file=__snapshot_file__,
-    old_graph=g)
+g = inc.load_kg(mapping_file=__mapping_file__,
+                snapshot_file=__snapshot_file__,
+                aux_data_path=__aux_data_path__,
+                old_graph=g,
+                method=__method__,
+                engine=__engine__)
 end = time.time()
 
-print("Loaded new graph in %.2fs" % (end-start))
+update_time = end-start
+update_triples = len(g)
 
-print("#" * 60)
+print("Loaded new triples in %.2fs" % (end-start))
 
-print("Restoring %s..." % __update_file__)
+print("Restoring data sources...")
+for file in updated_files:
+    print("\tRestoring %s... " % file, end='')
+    __restore_data(file, updated_files[file], file_extensions[file])
+    print("OK")
 
-agency_df.to_csv(__update_file__, index=False)
-
-print("Restored %s." % __update_file__)
+print("Restored data sources.")
 
 if not __keep_snapshot__:
-    print("Deleting %s..." % __snapshot_file__)
+    print("Deleting %s... " % __snapshot_file__, end='')
     os.remove(__snapshot_file__)
-    print("Deleted snapshot file.")
+    print("OK")
+
+print("Summary:")
+print("\tCreated new version in %.2fs (%d triples)" % (new_version_time, new_version_triples))
+print("\tUpdated in %.2fs (%d triples)" % (update_time, update_triples))
