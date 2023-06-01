db4e59fcf1fb05ef2ca08b4d6fcca6036276208e
ershimen <jan.cerezo6@gmail.com>
2023-06-01 23:45:28 +0200

    Added tests for removed data with 'memory' arg

--- test/test_utils.py
+++ test/test_utils.py
@@ -27,6 +27,34 @@ def __store_data(file: str, data: object, extension: str):
         raise NotImplementedError(f'The file type {extension} is not supported yet!')
 
 
+def __remove_data(file: str, data: object, extension: str):
+    """Removes 'data' from 'file' and returns the data in 'file' prior to the change.
+
+    Args:
+        file:
+            A file name.
+        data:
+            A data object (for example json). The types supported are the following:
+                .json: for .csv files
+        extension:
+            Extension of 'file'. This parameter determines how to open 'file' and the return type. 
+        
+    Returns:
+        The data in 'file' prior to the update. The type of the object returned depends 'extension':
+            .csv: returns pandas.DataFrame
+    """
+    if extension == '.csv':
+        df = pd.read_csv(file, dtype=str)
+        aux_data = pd.DataFrame([data])
+        df_new = pd.concat([df, aux_data, aux_data]).drop_duplicates(keep=False)
+        df_new.to_csv(file, index=False)
+        return df
+    elif extension == '.json':
+        raise NotImplementedError(f'The file type {extension} is not supported yet!')
+    else:
+        raise NotImplementedError(f'The file type {extension} is not supported yet!')
+
+
 def __restore_data(file: str, data: object, extension: str):
     """Restores 'data' to 'file'.
 
